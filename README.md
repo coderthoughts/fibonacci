d2901b62e22f38307b720fbf1947b0d828a13008
David Bosschaert <bosschae@adobe.com>
2018-01-29 21:46:30 +0000

    Fix up indentation

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# fibonacci
+To use this contract:
+
+Send some data to it to compute:
+./eosc push message david1 compute '{"ival":16}' --scope david1
+
+The result of the computation (33 in this case) will be stored in a table:
+$ ./eosc get table  david1 david1 results
+{
+  "rows": [{
+      "id": "david1",
+      "value": 33
+    }
+  ],
+  "more": false
+}
+
--- fibonacci.cpp
+++ fibonacci.cpp
@@ -10,41 +10,41 @@ extern "C" {
     };
 
     struct PACKED(result) {
-		result() {};
-		result(eosio::name id, uint64_t value):id(id), value(value) {};
+        result() {};
+        result(eosio::name id, uint64_t value):id(id), value(value) {};
         eosio::name id;
         uint64_t value;
     };
 
     using Results = eosio::table<N(fibonacci), N(fibonacci), N(results), result, uint64_t>;
 
-	uint64_t fibonacci(uint64_t iterations) {
-		uint64_t first = 0;
-		uint64_t second = 1;
+    uint64_t fibonacci(uint64_t iterations) {
+        uint64_t first = 0;
+        uint64_t second = 1;
 
-		if (iterations == 0)
-			return 0;
+        if (iterations == 0)
+            return 0;
 
-		eosio::print("1 ");
-		for (uint64_t i=1; i < iterations; i++) {
-			uint64_t res = first + second;
-			first = second;
-			second = res;
-			eosio::print(res, " ");
-		}
-		return second;
-	}
+        eosio::print("1 ");
+        for (uint64_t i=1; i < iterations; i++) {
+            uint64_t res = first + second;
+            first = second;
+            second = res;
+            eosio::print(res, " ");
+        }
+        return second;
+    }
 
     /// The apply method implements the dispatch of events to this contract
     void apply( uint64_t code, uint64_t action ) {
-		if (action == N(compute)) {
-			auto message = eosio::current_message<compute>();
-			eosio::print("Calling fibonacci\n");
-			uint64_t num = fibonacci(message.iterations);
+        if (action == N(compute)) {
+            auto message = eosio::current_message<compute>();
+            eosio::print("Calling fibonacci\n");
+            uint64_t num = fibonacci(message.iterations);
 
-			result res(eosio::name(code), num);
-			Results::store(res, res.id);
-			eosio::print("Stored result in database\n");
-		}
+            result res(eosio::name(code), num);
+            Results::store(res, res.id);
+            eosio::print("Stored result in database\n");
+        }
     }
 } // extern "C"
